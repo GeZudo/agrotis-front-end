755104cab01e4c5c1c0913395e384eea5ac39cbc
Augusto Alves <85210880+GeZudo@users.noreply.github.com>
2022-05-18 18:23:23 -0300

    ✨ Feat: Form Input Laboratorio

---
+++ src/helpers/labOptions.js
@@ -0,0 +1,28 @@
+const labs = [
+  {
+    id: 1,
+    nome: 'Agro Skynet',
+  },
+  {
+    id: 2,
+    nome: 'Umbrella Agro',
+  },
+  {
+    id: 3,
+    nome: 'Osborn Agro',
+  },
+  {
+    id: 4,
+    nome: 'Skyrim Agro',
+  },
+  {
+    id: 5,
+    nome: 'Agro Brasil',
+  },
+  {
+    id: 6,
+    nome: 'Quarenta e Dois Agro',
+  },
+];
+
+export default labs;
