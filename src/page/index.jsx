7ccb93f73fc5d64b41971ce87052a3088bec9ab2
Augusto Alves <85210880+GeZudo@users.noreply.github.com>
2022-05-18 18:15:36 -0300

    ✨ Feat: Form Input Data Inicial

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -3,12 +3,15 @@ import {
   Grid, Paper, TextField,
 } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { useState } from 'react';
 import FormHeader from '../components/FormHeader';
 import agrotis from '../theme';
 
 export default function MaindPage() {
   const [nome, setNome] = useState(null);
+  const [dataInicial, setDataInicial] = useState(null);
 
   const handleNameChange = ({ target: { value } }) => {
     setNome(value);
@@ -29,6 +32,17 @@ export default function MaindPage() {
               onChange={handleNameChange}
               value={nome}
             />
+          </Grid>
+          <Grid item xs={3}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Data Inicial"
+                value={dataInicial}
+                onChange={(newValue) => { setDataInicial(newValue); }}
+                renderInput={(params) => <TextField {...params} variant="standard" fullWidth required />}
+                required
+              />
+            </LocalizationProvider>
           </Grid>
         </Grid>
       </ThemeProvider>
